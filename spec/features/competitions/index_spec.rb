745ca6104362e2e5e04dcd89bca85c0e9cd767a0
Saundra <saundracatalina@gmail.com>
2021-01-19 09:50:32 -0700

    Add test can see names of all competitions

---
+++ spec/features/competitions/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "competition index" do
+  before :each do
+    @comp1 = Competition.create!(name: "South Side Comp", location: "NYC", sport: "Basketball")
+    @comp2 = Competition.create!(name: "North Side Comp", location: "Albany", sport: "Tennis")
+    @comp3 = Competition.create!(name: "West Side Comp", location: "Buffalo", sport: "Hockey")
+  end
+  it "can see the names of all competitions" do
+    visit competitions_path
+
+    expect(page).to have_content(@comp1.name)
+    expect(page).to have_content(@comp2.name)
+    expect(page).to have_content(@comp3.name)
+  end
+  it "each competition name links to it's show page"
+end
