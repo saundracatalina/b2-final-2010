45915836ec2d02594d48fdd8ec8740955adb6e03
Saundra <saundracatalina@gmail.com>
2021-01-19 13:48:37 -0700

    Refactor average player age method to utilize relationships and use only AR

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,10 +1,9 @@
 class Competition < ApplicationRecord
   has_many :competition_teams
   has_many :teams, through: :competition_teams
+  has_many :players, through: :teams
 
   def ave_players_age
-    x = teams.joins(:players).pluck(:age)
-    y = (x.sum/x.count)
-    y
+    players.average(:age).to_f.round(2)
   end
 end
